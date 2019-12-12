da7230836ff38ee52851a512b268d5555c7d1eb2
Zhengyu Duan <670450301@qq.com>
2019-12-12 15:42:17 -0800

    deleted echo

--- cookie_section/set_last_5_visited.php
+++ cookie_section/set_last_5_visited.php
@@ -4,10 +4,10 @@
 echo $product_name."\n";
 if($_COOKIE[$product_name]== null){
 	setcookie($product_name,1,time()+3600);
-  echo "set cooki\n";
+  // echo "set cooki\n";
 }else{
 	setcookie($product_name,$_COOKIE[$product_name]+1,time()+3600);
-  echo "refresh cooki\n";
+  // echo "refresh cooki\n";
 }
 if ($_COOKIE["viewed1"] != $product_name ){
 setcookie("viewed5", $_COOKIE["viewed4"], time()+3600);
@@ -16,9 +16,9 @@ setcookie("viewed3", $_COOKIE["viewed2"], time()+3600);
 setcookie("viewed2", $_COOKIE["viewed1"], time()+3600);
 setcookie("viewed1", $product_name, time()+3600);
 
-echo "displaying cookie name:".$_COOKIE["viewed1"]."\n";
+// echo "displaying cookie name:".$_COOKIE["viewed1"]."\n";
 
 }
 
-echo "???";
+// echo "???";
 ?>
