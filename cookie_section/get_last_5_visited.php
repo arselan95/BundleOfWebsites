723e0aebd43272b726de5d31d9b9b85616de6317
AquiLAlpha <zdq940816@yeah.net>
2019-12-12 16:32:51 -0800

    set get cookie

--- cookie_section/get_last_5_visited.php
+++ cookie_section/get_last_5_visited.php
@@ -1,21 +1,26 @@
 <?php $title = 'Visited'; ?>
 <!-- <?php include('header.php');?> -->
 
-<div style="padding:20px;margin-top:30px;height:1500px;">
+<div>
 <h1>Last 5 Visited Product</h1>
-<table border ="5" cellspacing = "0" cellpadding="10">
+<table border ="5" cellspacing = "0" cellpadding="10" class="review">
 	<tr><th>Order</th><th>Product Name</th></tr>
 
   <?php
 
-    if($_COOKIE['viewed1'])print( "<tr><td>Last viewed 1</td><td> $_COOKIE[viewed1]</td>");
-    if($_COOKIE['viewed2'])print( "<tr><td>Last viewed 2</td><td> $_COOKIE[viewed2]</td>");
-    if($_COOKIE['viewed3'])print( "<tr><td>Last viewed 3</td><td> $_COOKIE[viewed3]</td>");
-    if($_COOKIE['viewed4'])print( "<tr><td>Last viewed 4</td><td> $_COOKIE[viewed4]</td>");
-    if($_COOKIE['viewed5'])print( "<tr><td>Last viewed 5</td><td> $_COOKIE[viewed5]</td>");
+    if(array_key_exists('viewed1', $_COOKIE)&&$_COOKIE['viewed1'])
+        print( "<tr><td>Last viewed 1</td><td> $_COOKIE[viewed1]</td>");
+    if(array_key_exists('viewed2', $_COOKIE)&&$_COOKIE['viewed2'])
+        print( "<tr><td>Last viewed 2</td><td> $_COOKIE[viewed2]</td>");
+    if(array_key_exists('viewed3', $_COOKIE)&&$_COOKIE['viewed3'])
+        print( "<tr><td>Last viewed 3</td><td> $_COOKIE[viewed3]</td>");
+    if(array_key_exists('viewed4', $_COOKIE)&&$_COOKIE['viewed4'])
+        print( "<tr><td>Last viewed 4</td><td> $_COOKIE[viewed4]</td>");
+    if(array_key_exists('viewed5', $_COOKIE)&&$_COOKIE['viewed5'])
+        print( "<tr><td>Last viewed 5</td><td> $_COOKIE[viewed5]</td>");
   ?>
 </table>
 
 
-<a href = "clear_last_5_visited.php">Clear History</a>
+<a href = "cookie_section/clear_last_5_visited.php">Clear History</a>
 </div>
