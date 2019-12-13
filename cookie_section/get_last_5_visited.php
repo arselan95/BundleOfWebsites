887a72eef427361ac29bdb166a03ba9c7e654bd9
Zhengyu Duan <670450301@qq.com>
2019-12-12 16:02:11 -0800

    fixed undefined

--- cookie_section/get_last_5_visited.php
+++ cookie_section/get_last_5_visited.php
@@ -8,11 +8,11 @@
 
   <?php
 
-    if($_COOKIE[viewed1])print( "<tr><td>Last viewed 1</td><td> $_COOKIE[viewed1]</td>");
-    if($_COOKIE[viewed2])print( "<tr><td>Last viewed 2</td><td> $_COOKIE[viewed2]</td>");
-    if($_COOKIE[viewed3])print( "<tr><td>Last viewed 3</td><td> $_COOKIE[viewed3]</td>");
-    if($_COOKIE[viewed4])print( "<tr><td>Last viewed 4</td><td> $_COOKIE[viewed4]</td>");
-    if($_COOKIE[viewed5])print( "<tr><td>Last viewed 5</td><td> $_COOKIE[viewed5]</td>");
+    if($_COOKIE['viewed1'])print( "<tr><td>Last viewed 1</td><td> $_COOKIE[viewed1]</td>");
+    if($_COOKIE['viewed2'])print( "<tr><td>Last viewed 2</td><td> $_COOKIE[viewed2]</td>");
+    if($_COOKIE['viewed3'])print( "<tr><td>Last viewed 3</td><td> $_COOKIE[viewed3]</td>");
+    if($_COOKIE['viewed4'])print( "<tr><td>Last viewed 4</td><td> $_COOKIE[viewed4]</td>");
+    if($_COOKIE['viewed5'])print( "<tr><td>Last viewed 5</td><td> $_COOKIE[viewed5]</td>");
   ?>
 </table>
 
